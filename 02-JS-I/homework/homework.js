8660ef336c55cfaab2f232f61affcee76b64a52f
Ezequiel <gomezmerloe@gmail.com>
2022-08-23 10:05:05 -0300

    hola

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'gato';
 const nuevoNum = 7;
 
 // Crea una variable booleana:
-const nuevoBool = true;
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
